712e5a5b72fc0e819554448f31b699c378f5a9a3
Nathan Tjahjadi <nathantja@yahoo.com>
2023-11-06 17:47:36 -0600

    finished further study

--- dsa-stacks-queues/further-study/further-study.js
+++ dsa-stacks-queues/further-study/further-study.js
@@ -1,11 +1,74 @@
 const Queue = require("../queue");
 const Stack = require("../stack");
 
+
+/** Reverse string without using an array. */
 function stringReversal(str) {
+  const stack = new Stack();
+
+  for (const letter of str) {
+    stack.push(letter);
+  }
+
+  let reversedStr = "";
+
+  while (stack.isEmpty() === false) {
+    reversedStr = reversedStr.concat(stack.pop());
+  }
+
+  return reversedStr;
+}
+
+/**  */
+function balancedBrackets1(str) {
+  const stack = new Stack();
+  const openBrackets = "({[";
+  const closeBrackets = ")}]";
+
+
+  for (const char of str) {
+    if (openBrackets.includes(char)) stack.push(char);
+
+    if (closeBrackets.includes(char)) {
+      if (!stack.isEmpty()) top = stack.peek();
+      else return false;
+
+      if (char === ")" && top === "(") stack.pop();
+      else if (char === "}" && top === "{") stack.pop();
+      else if (char === "]" && top === "[") stack.pop();
+      else return false;
+    }
+  }
+
+  return stack.isEmpty();
 }
 
 function balancedBrackets(str) {
+  const stack = new Stack();
+  const openBrackets = "({[";
+  const closeBrackets = ")}]";
+
+  for (const char of str) {
+
+    if (openBrackets.includes(char)) {
+      if (char === "(") stack.push(")");
+      else if (char === "{") stack.push("}");
+      else if (char === "[") stack.push("]");
+    }
+
+    if (closeBrackets.includes(char)) {
+      if (!stack.isEmpty()) top = stack.peek();
+      else return false;
+
+      if (top === char) stack.pop();
+    }
+  }
+
+  return stack.isEmpty();
 }
+
+
+
 
 function josephusSurvivor(peopleNum, skip) {
 }
